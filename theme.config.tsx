e2c375ccad3c69a742de79f48abffc1cf4cbb23c
pl3lee <billy.pl.lee@gmail.com>
2024-02-15 05:16:15 +0000

    edited footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   // },
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Documentation',
+    text: `@ ${new Date().getFullYear()} Billy Lee. All rights reserved.`
   },
 }
 
